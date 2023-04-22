f8100944bc276fe8fd606d18e96d5dc27ee15efa
Yaduk035 <122448090+Yaduk035@users.noreply.github.com>
2023-04-22 21:21:40 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,95 +1,68 @@
-// Task1: initiate app and run server at 3000
+// Task 1: initiate app and run server at 3000
+const Express = require("express")
+const BodyParser = require("body-parser")
+const Cors = require("cors")
+const {employeeModel} = require("./model/Employee")
+const Mongoose = require("mongoose")
 
-const express = require('express');
-const app = express();
+const app = Express()
+
+app.use(Cors())
+app.use(BodyParser.json())
+app.use(BodyParser.urlencoded({extended : true}))
+
 const path=require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
+app.use(Express.static(path.join(__dirname+'/dist/FrontEnd')));
 
-const port = 3000;
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
-
-// Task2: create mongoDB connection 
+// Task 2: create mongoDB connection
+Mongoose.connect("mongodb+srv://yaduk035:Metalhead3063.@cluster0.bygqnjf.mongodb.net/db1?retryWrites=true&w=majority", {useNewUrlParser: true})
 
 
-const url = 'mongodb+srv://username:<password>@cluster0.bygqnjf.mongodb.net/testdb?retryWrites=true&w=majority';
-const client = new MongoClient(url, { useNewUrlParser: true });
-
-client.connect(err => {
-  const collection = client.db("testdb").collection("merns");
-
-
-// //Task 2 : write api with error handling and appropriate api mentioned in the TODO below
-
-
-app.get('/api/employeelist', (req, res) => {
-    collection.find({}).toArray((err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
-  });
-
+//Task 3 : write api with error handling and appropriate api mentioned in the TODO below
 //TODO: get data from db  using api '/api/employeelist'
-
-app.get('/api/employeelist/:id', (req, res) => {
-    const id = req.params.id;
-    collection.findOne({ _id: id }, (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
-  });
-// TODO: Get a single employee from the database
-
-
+app.get('/api/employeelist', async (req, res) => {
+    var data = await employeeModel.find()
+    res.json(data)
+})
 
 //TODO: get single data from db  using api '/api/employeelist/:id'
-
-
+app.get('/api/employeelist/:id', async (req, res) => {
+   const id = req.params.id
+   var data = await employeeModel.findById(id)
+   res.json(data)
+})
 
 //TODO: send data from db using api '/api/employeelist'
 //Request body format:{name:'',location:'',position:'',salary:''}
-
-app.post('/api/employeelist', (req, res) => {
-    const employee = req.body;
-    collection.insertOne(employee, (err, result) => {
-      if (err) throw err;
-      res.send('Employee added to database');
-    });
-  });
-
-
+app.post('/api/employeelist', async (req, res) => {
+    var data = new employeeModel(req.body)
+    data.save()
+    res.redirect('/api/employeelist')
+})
 
 //TODO: delete a employee data from db by using api '/api/employeelist/:id'
-
-app.delete('/api/employeelist/:id', (req, res) => {
-    const id = req.params.id;
-    collection.deleteOne({ _id: id }, (err, result) => {
-      if (err) throw err;
-      res.send('Employee deleted from database');
-    });
-  });
+app.delete('/api/employeelist/:id', async (req, res) => {
+    const id = req.params.id
+    var data = await employeeModel.findByIdAndDelete(id)
+    res.json(data)
+ })
 
 //TODO: Update  a employee data from db by using api '/api/employeelist'
 //Request body format:{name:'',location:'',position:'',salary:''}
-
-app.put('/api/employeelist', (req, res) => {
-    const employee = req.body;
-    collection.updateOne({ _id: employee._id }, { $set: employee }, (err, result) => {
-      if (err) throw err;
-      res.send('Employee updated in database');
-    });
-  });
+app.put('/api/employeelist/', async (req, res) => {
+    await employeeModel.findOneAndUpdate({"_id": req.body._id}, req.body)
+    res.json({status: "Success"})
+})
 
 
 //! dont delete this code. it connects the front end file.
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
 });
+
+app.listen(3000, () => {
+    console.log('Server Started')
+})
 
 
 
